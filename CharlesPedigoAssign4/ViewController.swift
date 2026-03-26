b6ddbf838cebcc911b836f9765a2507e0e4ecfb3
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-03-25 20:38:28 -0400

    UI and Outlet/Actions implemented

--- CharlesPedigoAssign4/ViewController.swift
+++ CharlesPedigoAssign4/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var TipSplitView: UIView!
+    
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var tipSliderLabel: UILabel!
+    
+    @IBOutlet weak var peopleStepper: UIStepper!
+    @IBOutlet weak var peopleStepperLabel: UILabel!
+    
+    @IBOutlet weak var calculationLabel: UILabel!
+    @IBOutlet weak var generosityLabel: UILabel!
+    
+    @IBOutlet weak var confirmationImage: UIImageView!
+    
+    
+    //Actions
+    @IBAction func tipSliderChanged(_ sender: Any) {
+    }
+    
+    @IBAction func peopleStepperChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculateButton(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TipSplitView.layer.cornerRadius = 20
+        
     }
 
 
